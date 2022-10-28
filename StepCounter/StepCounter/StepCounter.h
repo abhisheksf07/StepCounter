95f3cc72d208f6e3857eea3f35f97d5a8bb5c515
abhishek singla <abhishek.singla@sourcefuse.com>
2022-10-28 16:20:38 +0530

    Finished our StepCounter framework

---
+++ StepCounter/StepCounter/StepCounter.h
@@ -0,0 +1,18 @@
+//
+//  StepCounter.h
+//  StepCounter
+//
+//  Created by Abhishek  Singla on 28/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StepCounter.
+FOUNDATION_EXPORT double StepCounterVersionNumber;
+
+//! Project version string for StepCounter.
+FOUNDATION_EXPORT const unsigned char StepCounterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StepCounter/PublicHeader.h>
+
+
